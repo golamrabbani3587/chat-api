b4698b9a7e6dd2d8d687eef26c7364db6a202d26
Md. Golam Rabbani <md.golamrabbani@Mds-MacBook-Air.local>
2023-12-26 14:58:05 +0600

    f

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(cors());
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.emit('hi');
+});
+
+server.listen(3001, () => {
+  console.log('Express server listening on *:3001');
+});
